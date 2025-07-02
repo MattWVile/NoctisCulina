aa3d4a5df3f470c9155b9d5fce6472d8e58d4f6d
Matt Vile <matt.vile@yahoo.co.uk>
2025-07-02 10:15:24 +0100

    added tower preview

--- Assets/Scripts/Towers/BuildManager.cs
+++ Assets/Scripts/Towers/BuildManager.cs
@@ -9,6 +9,11 @@ public class BuildManager : MonoBehaviour
 
     private TowerData selectedTower;
     private bool isBuildMode = false;
+    private GameObject previewInstance;
+    [Header("Preview Settings")]
+    [SerializeField, Range(0f, 1f)] private float previewAlpha = 0.2f;
+    [SerializeField] private Color unaffordableColor = new Color(0.6886792f, 0.2111517f, 0.2111517f, 0.4627451f); // semi-transparent red
+    [SerializeField] private Color previewcolour = new Color(.5f, 1f, 1f, 0.4627451f); // semi-transparent red
 
     private void Awake()
     {
@@ -29,23 +34,39 @@ public class BuildManager : MonoBehaviour
         if (!isBuildMode)
         {
             selectedTower = null;
+            DestroyPreview();
         }
     }
 
     public void SelectTowerToBuild(TowerData tower)
     {
         selectedTower = tower;
+        CreatePreview();
     }
 
     private void Update()
     {
         if (!isBuildMode)
+        {
+            DestroyPreview();
             return;
+        }
 
         // Tower selection input
         if (Input.GetKeyDown(KeyCode.Alpha1) && availableTowers.Length > 0)
         {
             SelectTowerToBuild(availableTowers[0]);
+        }
+
+        // Update preview position and color
+        if (previewInstance != null)
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+            previewInstance.transform.position = mouseWorldPos;
+
+            bool canAfford = PlayerResources.Instance != null && selectedTower != null && PlayerResources.Instance.Resources >= selectedTower.cost;
+            UpdatePreviewColor(canAfford);
         }
 
         // Place tower on left mouse click
@@ -62,11 +83,64 @@ public class BuildManager : MonoBehaviour
         if (PlayerResources.Instance != null && PlayerResources.Instance.SpendResources(selectedTower.cost)) 
         {
             Instantiate(selectedTower.towerPrefab, position, Quaternion.identity);
-            // TODO: Play build sound, animation, etc.
+            // Optionally: Play build sound, animation, etc.
+            // Keep preview if still affordable, else tint red
+            if (PlayerResources.Instance.Resources < selectedTower.cost)
+            {
+                UpdatePreviewColor(false);
+            }
         }
         else
         {
-            // TODO: Show not enough resources UI
+            // Optionally: Show not enough resources UI
+            UpdatePreviewColor(false);
+        }
+    }
+
+    private void CreatePreview()
+    {
+        DestroyPreview();
+        if (selectedTower != null && selectedTower.towerPrefab != null)
+        {
+            previewInstance = Instantiate(selectedTower.towerPrefab);
+            previewInstance.name = "Preview_" + selectedTower.towerPrefab.name;
+            // Make all SpriteRenderers semi-transparent (original color, just lower alpha)
+            foreach (var sr in previewInstance.GetComponentsInChildren<SpriteRenderer>())
+            {
+                sr.color = previewcolour;
+            }
+            // Disable any scripts/colliders on the preview
+            foreach (var comp in previewInstance.GetComponents<MonoBehaviour>())
+                comp.enabled = false;
+            foreach (var col in previewInstance.GetComponentsInChildren<Collider2D>())
+                col.enabled = false;
+        }
+    }
+
+    private void UpdatePreviewColor(bool canAfford)
+    {
+        if (previewInstance == null) return;
+        foreach (var sr in previewInstance.GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (canAfford)
+            {
+                Color orig = sr.color;
+                orig.a = previewAlpha;
+                sr.color = new Color(orig.r, orig.g, orig.b, previewAlpha);
+            }
+            else
+            {
+                sr.color = unaffordableColor;
+            }
+        }
+    }
+
+    private void DestroyPreview()
+    {
+        if (previewInstance != null)
+        {
+            Destroy(previewInstance);
+            previewInstance = null;
         }
     }
 }
