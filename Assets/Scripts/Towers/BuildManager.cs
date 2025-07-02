fa07c59107ac3ea79345d3f75a3380fe281c4abe
Matt Vile <matt.vile@yahoo.co.uk>
2025-07-02 10:55:25 +0100

    removed line renderer from preview

--- Assets/Scripts/Towers/BuildManager.cs
+++ Assets/Scripts/Towers/BuildManager.cs
@@ -10,10 +10,10 @@ public class BuildManager : MonoBehaviour
     private TowerData selectedTower;
     private bool isBuildMode = false;
     private GameObject previewInstance;
+    private SpriteRenderer previewRangeRenderer;
     [Header("Preview Settings")]
     [SerializeField, Range(0f, 1f)] private float previewAlpha = 0.2f;
     [SerializeField] private Color unaffordableColor = new Color(0.6886792f, 0.2111517f, 0.2111517f, 0.4627451f); // semi-transparent red
-    [SerializeField] private Color previewColor = new Color(.5f, 1f, 1f, 0.4627451f); // semi-transparent preview color
 
     private void Awake()
     {
@@ -100,35 +100,51 @@ public class BuildManager : MonoBehaviour
     private void CreatePreview()
     {
         DestroyPreview();
+        previewRangeRenderer = null;
         if (selectedTower != null && selectedTower.towerPrefab != null)
         {
-            previewInstance = Instantiate(selectedTower.towerPrefab);
-            previewInstance.name = "Preview_" + selectedTower.towerPrefab.name;
-            // Make all SpriteRenderers semi-transparent (original color, just lower alpha)
-            foreach (var sr in previewInstance.GetComponentsInChildren<SpriteRenderer>())
+            // Create an empty GameObject for the preview
+            previewInstance = new GameObject("Preview_" + selectedTower.towerPrefab.name);
+            // Copy all SpriteRenderers from the prefab (including children)
+            var prefabRenderers = selectedTower.towerPrefab.GetComponentsInChildren<SpriteRenderer>(true);
+            foreach (var prefabRenderer in prefabRenderers)
             {
-                sr.color = previewColor;
+                // Create a child GameObject for each sprite
+                GameObject child = new GameObject(prefabRenderer.gameObject.name);
+                child.transform.SetParent(previewInstance.transform);
+                child.transform.localPosition = prefabRenderer.transform.localPosition;
+                child.transform.localRotation = prefabRenderer.transform.localRotation;
+                child.transform.localScale = prefabRenderer.transform.localScale;
+                var sr = child.AddComponent<SpriteRenderer>();
+                sr.sprite = prefabRenderer.sprite;
+                sr.sortingLayerID = prefabRenderer.sortingLayerID;
+                sr.sortingOrder = prefabRenderer.sortingOrder;
+                sr.flipX = prefabRenderer.flipX;
+                sr.flipY = prefabRenderer.flipY;
+                sr.color = prefabRenderer.color;
+                sr.material = prefabRenderer.sharedMaterial;
+                // If this is the range indicator, store a reference
+                if (prefabRenderer.GetComponent<RangeController>() != null || prefabRenderer.gameObject.name.ToLower().Contains("range"))
+                {
+                    previewRangeRenderer = sr;
+                }
             }
-            // Disable any scripts/colliders on the preview
-            foreach (var comp in previewInstance.GetComponents<MonoBehaviour>())
-                comp.enabled = false;
-            foreach (var col in previewInstance.GetComponentsInChildren<Collider2D>())
-                col.enabled = false;
         }
     }
 
     private void UpdatePreviewColor(bool canAfford)
     {
         if (previewInstance == null) return;
-        foreach (var sr in previewInstance.GetComponentsInChildren<SpriteRenderer>())
+        if (previewRangeRenderer != null)
         {
             if (canAfford)
             {
-                sr.color = previewColor;
+                // Restore original color (no tint)
+                // (Assume the color is already correct from CreatePreview)
             }
             else
             {
-                sr.color = unaffordableColor;
+                previewRangeRenderer.color = unaffordableColor;
             }
         }
     }
